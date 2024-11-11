14db2a309b45e2c1e2108df578c341e6ef3021db
Himanshu0422 <himanshumittal035@gmail.com>
2024-11-12 00:50:56 +0530

    change

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -3,19 +3,12 @@ import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
     try {
-        await connectToDB();
-        
-        console.log("Connected to DB in production"); // Debug connection
+        await connectToDB()
 
-        const prompts = await Prompt.find({}).populate('creator');
-        console.log("Fetched prompts:", prompts); // Debug data retrieval
+        const prompts = await Prompt.find({}).populate('creator')
 
-        return new Response(JSON.stringify(prompts), {
-            status: 200,
-            headers: { "Cache-Control": "no-store" }
-        });
+        return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        console.error("Error fetching prompts:", error); // Debug error
-        return new Response("Failed to fetch all prompts", { status: 500 });
+        return new Response("Failed to fetch all prompts", { status: 500 })
     }
-};
+} 
