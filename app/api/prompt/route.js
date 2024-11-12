3f9da3de8472dd239f42ea928253a47f3ccc2c71
Himanshu0422 <himanshumittal035@gmail.com>
2024-11-12 23:59:01 +0530

    dynamic data

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -9,36 +9,9 @@ export const GET = async (request) => {
         const prompts = await Prompt.find({}).populate('creator')
         console.log(prompts, 'prompts');
 
-        return new Response(JSON.stringify([
-            {
-              "_id": "67332956f4a3c3382b51c3c6",
-              "creator": {
-                "_id": "67332947f4a3c3382b51c3c2",
-                "email": "himanshu.enact@gmail.com",
-                "username": "himanshumittal",
-                "image": "https://lh3.googleusercontent.com/a/ACg8ocIWRpanxXHILGLuMqYOxB6B_7Wgd3v52LPFTGwBg24ONV2V3g=s96-c",
-                "__v": 0
-              },
-              "prompt": "Himanshu",
-              "tag": "fourth",
-              "__v": 0
-            },
-            {
-              "_id": "673386d72b9b167c63b7bc37",
-              "creator": {
-                "_id": "673252899d91d391dc7d3ba9",
-                "email": "himanshumittal035@gmail.com",
-                "username": "himanshumittal",
-                "image": "https://lh3.googleusercontent.com/a/ACg8ocLZYQRN1SLG4PL5imexMja6AFE-VfpfPlaRufdudluzjZx8Fw=s96-c",
-                "__v": 0
-              },
-              "prompt": "Fourth",
-              "tag": "fourth",
-              "__v": 0
-            }
-          ]
-          ), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
+      console.error(error);
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
 } 
